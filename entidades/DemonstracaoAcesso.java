3f91441834981e3e6aa4e84990ed52ef8ab82e7a
Gustavo <129295616+GustavoAnacleto@users.noreply.github.com>
2026-06-13 14:36:11 -0300

    feat: adiciona demonstracao de modificadores de acesso

---
+++ entidades/DemonstracaoAcesso.java
@@ -0,0 +1,22 @@
+package entidades;entidades
+
+public class DemonstracaoAcesso{
+
+	public String publico;
+	private String privado;
+	protected String protegido;
+	String padrao;
+
+	public void exibirDentroDaClasse(){
+
+		publico = "Atributo público acessado dentro da própria classe";
+		privado = "Atributo privado acessado dentro da própria classe";
+		protegido = "Atributo protegido acessado dentro da própria classe";
+        padrao = "Atributo padrão acessado dentro da própria classe";
+
+        System.out.println(publico);
+        System.out.println(privado);
+        System.out.println(protegido);
+        System.out.println(padrao);
+	}
+}
